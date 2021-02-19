e4be8a78ca95d24f395641ad6f00b8ca16bbd0f4
unknown <nidalmessaoudi@hotmail.com>
2021-02-20 00:45:56 +0100

    Project Files

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = () => {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closeModal = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
